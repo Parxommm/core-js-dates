114581c13c63f059fb593086b964280f277d9041
Evgeny Parkhomenko <evg.parkhomenko@gmail.com>
2024-03-24 17:14:29 -0300

    fix: fix getDayName

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -50,7 +50,26 @@ function getTime(date) {
  * '2024-01-30T00:00:00.000Z' => 'Tuesday'
  */
 function getDayName(date) {
-  return new Date(date).toLocaleDateString('en-US', { weekday: 'long' });
+  const day = new Date(date).getUTCDay();
+
+  switch (day) {
+    case 0:
+      return 'Sunday';
+    case 1:
+      return 'Monday';
+    case 2:
+      return 'Tuesday';
+    case 3:
+      return 'Wednesday';
+    case 4:
+      return 'Thursday';
+    case 5:
+      return 'Friday';
+    case 6:
+      return 'Saturday';
+    default:
+      return undefined;
+  }
 }
 
 /**
